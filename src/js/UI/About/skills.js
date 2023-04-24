4a3ad690faab42f7ba0332565b3e2f99b9c73d35
minato <tigerrabbit22@outlook.com>
2023-04-23 21:46:55 -0400

    first push

---
+++ src/js/UI/About/skills.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'WebGL',
+        width: '60%',
+    },
+    {
+        name: 'ReactJS',
+        width: '65%',
+    },
+    {
+        name: 'JavaScript',
+        width: '85%',
+    },
+    {
+        name: 'HTML + CSS',
+        width: '90%'
+    },
+    {
+        name: '3D Modelling',
+        width: '60%',
+    },
+]
---
+++ src/js/UI/Work/tags.js
@@ -0,0 +1,9 @@
+export default {
+  html: `<div class="work-item-tag" style="background: white; border: 1px solid #7C8594; color: #7C8594">HTML</div>`,
+  css: `<div class="work-item-tag" style="background: white; border: 1px solid #7C8594; color: #7C8594">CSS</div>`,
+  javascript: `<div class="work-item-tag" style="background: #FFB800;">JavaScript</div>`,
+  socketio: `<div class="work-item-tag" style="background: #21BAEB;">Socket.IO</div>`,
+  webgl: `<div class="work-item-tag" style="background: #5A69EC;">WebGL</div>`,
+  api: `<div class="work-item-tag" style="background: #CA49F8;">API</div>`,
+  backend: `<div class="work-item-tag" style="background: #8433CC;">Backend</div>`,
+};
